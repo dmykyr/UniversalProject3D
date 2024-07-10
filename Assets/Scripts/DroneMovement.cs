b72e982c0e335f7fb1996e81f8d2a4f4624b757e
kropli <dimakirimail@gmail.com>
2024-07-10 23:52:56 +0300

    Enhancement: added friend drone npc
    - created drone smooth movement script
    - added drone object to levels

---
+++ Assets/Scripts/DroneMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed = 5f;
+    public float distanceOffset = 0.3f;
+    public float leftOffset = -1f;
+    public float heightOffset = 2f;
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position - target.forward * distanceOffset + target.right * leftOffset;
+            desiredPosition.y = target.position.y + heightOffset;
+
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}
